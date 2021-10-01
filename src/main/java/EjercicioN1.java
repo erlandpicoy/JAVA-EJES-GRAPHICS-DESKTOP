1f70b642bcd87bbc0d0fdbbf33b791bc86c42d6d
ivanpicoy <picoy.ivan@gmail.com>
2021-10-01 16:26:11 -0300

    EjercicioN1_swing

--- src/main/java/EjercicioN1.java
+++ src/main/java/EjercicioN1.java
@@ -1,2 +1,18 @@
-public class EjercicioN1 {
+import javax.swing.*;
+import java.text.Normalizer;
+
+public class EjercicioN1 extends JFrame {
+    private JLabel label1;
+    public EjercicioN1(){
+        setLayout(null);
+        label1 = new JLabel("A la grande le puse cuca");
+        label1.setBounds(0,0,400,500);
+        add(label1);
+    }
+    public static void main (String args []){
+        EjercicioN1 formulario1 = new EjercicioN1();
+        formulario1.setBounds(0,0,400,300);
+        formulario1.setVisible(true);
+        formulario1.setLocation(null);
+    }
 }
